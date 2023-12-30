0df3ff90852aa18c1d24c10b4acaafa91191b478
JuanEs-az <juarangoz@unal.edu.co>
2023-12-30 13:51:23 -0500

    Clase 43: break-continue

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+        Scanner console = new Scanner( System.in );
+        while( true ){
+            System.out.println("Ingrese su email:");
+            String email = console.nextLine();
+            if( email.equals("toor") ){
+                System.out.println("Este usuario está baneado.");
+                continue;
+            }
+            System.out.println("Ingrese su contraseña");
+            String password = console.nextLine();
+            if( email.equals("root") && password.equals("1234") ){
+                System.out.println("Welcome to la interfaz.");
+                break;
+            }
         }
     }
 }
